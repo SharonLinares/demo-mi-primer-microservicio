a25fb5d9252e255bc6f92e7a63fd2c90ec424477
Sharon Linares <akiralyn02@gmail.com>
2025-09-24 12:16:54 +0200

    mi tercer commit

--- src/main/java/com/example/demo/controller/ProductoController.java
+++ src/main/java/com/example/demo/controller/ProductoController.java
@@ -1,8 +1,14 @@
 package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.dto.ProductoDto;
 
 @RestController
 @RequestMapping("/productos")
@@ -12,4 +18,21 @@ public class ProductoController {
 	public String consultarProductos() {
 		return "toma tu lista de productos";
 	}
+	
+	@GetMapping("/consultarProductosPorCodigo")
+	public String consultarProductosPorCodigo(@RequestParam(required = false) String codigo) {
+		return "toma tu producto con codigo: " + codigo;
+	}
+	
+	@GetMapping("/consultarProductosPorNombre/{nombres}")
+	public String consultarProductosPorNombre(@PathVariable(name = "nombres") String nombre) {
+		return "toma tu producto con nombre: " + nombre;
+	}
+	
+	
+	@PostMapping("/crearProductos")
+	public String crearProductos(@RequestBody ProductoDto productoDto ) {
+		return "se ha creado el producto con el nombre: " + productoDto.getNombre();
+	}
+	
 }
